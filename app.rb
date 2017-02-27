cbdb4a0fc06aaa9fb506505c2b73321e872e9f9a
cypselusmurarius <cypselusmurarius@gmail.com>
2017-02-27 18:34:23 +0300

    Add /contacts(email and messages, add contacts.txt), upgrade visit.erb(add users.txt)

--- app.rb
+++ app.rb
@@ -15,6 +15,29 @@ get '/contacts' do
 	erb :contacts
 end
 
+post '/contacts' do
+	erb :contacts
+	@email = params[:email]
+
+	f1 = File.open './public/contacts.txt', 'a'
+	f1.write "Email: #{@email}\n"
+	f1.close
+
+	@contactsmessage = 'Вы успешно добавлены в контакты'
+end
+
 get '/visit' do
 	erb :visit
+end
+
+post '/visit' do
+	erb :visit
+	@username = params[:username]
+	@phone = params[:phone]
+	@date_and_time = params[:date_and_time]
+
+	f2 = File.open './public/users.txt', 'a'
+	f2.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@date_and_time}\n"
+	f2.close
+	@visitmessage = 'Вы успешно записались на стрижку'
 end
